bdebb0e7dacf5c5b66d6a351f5fa28d4cad9890f
Sascha Vusatjuk <sv-213849@hs-weingarten.de>
2026-05-21 13:33:16 +0200

    Add Spring Boot backend skeleton

---
+++ backend/src/main/java/de/rwu/swa/bewerbungstracker/BewerbungstrackerApplication.java
@@ -0,0 +1,13 @@
+package de.rwu.swa.bewerbungstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BewerbungstrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BewerbungstrackerApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/rwu/swa/bewerbungstracker/BewerbungstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.rwu.swa.bewerbungstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BewerbungstrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
